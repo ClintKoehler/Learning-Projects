08d8e2c523138da327c4a4384e65068bd8fdfe59
Clint Koehler <clint.koehler@gmail.com>
2022-07-26 15:58:09 -0700

    into to js

---
+++ 100days/Javascript/Exercises/app.js
@@ -0,0 +1,42 @@
+// let age = 32;
+// let userName = 'Max';
+// let hobbies = ['Skiing', 'Cooking', 'Reading'];
+// let job = {
+//   title: 'Developer',
+//   place: 'New York',
+//   salary: 50000,
+// };
+
+// let adultYears;
+
+// function calculateAdultYears(userAge) {
+//   return userAge - 18;
+// }
+
+// adultYears = calculateAdultYears(age);
+
+// alert(adultYears);
+
+// let course = 'Math';
+// let cost = 200;
+// let goals = ['learn', 'study', 'graduate'];
+
+// let courseInfo = {
+//   course: 'Math',
+//   cost: 200,
+//   goals: ['learn', 'study', 'graduate'],
+// };
+
+// console.log(
+//   courseInfo.course + ' ' + courseInfo.cost + ' ' + ' ' + courseInfo.goals
+// );
+// console.log(courseInfo.goals[1]);
+
+// function printGoals(array, arrayIndex) {
+//   let arrayElement = array[arrayIndex];
+//   return arrayElement;
+// }
+
+// let thirdGoal = printGoals(courseInfo.goals, 2);
+
+// console.log(thirdGoal);
