4e666d3c9fd6bd7683c02184e4b5ea4f146d72fd
Clint Koehler <clint.koehler@gmail.com>
2022-07-27 12:07:48 -0700

    dom exercises

--- 100days/Javascript/Exercises/app.js
+++ 100days/Javascript/Exercises/app.js
@@ -40,3 +40,15 @@
 // let thirdGoal = printGoals(courseInfo.goals, 2);
 
 // console.log(thirdGoal);
+
+// let userName = ['Chewbacca', 'Catface', 'Max', 'Sherry'];
+// console.log(userName.length);
+
+document.body.children[1].children[0].href = 'https://google.com';
+
+// // in chrome dev tools
+// document.body.firstChild;
+// document.body.firstElementChild;
+// document.body.children[];
+// document.body.childNodes;
+// //
