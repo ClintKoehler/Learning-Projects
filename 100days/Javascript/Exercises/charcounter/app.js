7a782995ec224ddf174001391b8a2e561492d194
Clint Koehler <clint.koehler@gmail.com>
2022-07-28 13:54:44 -0700

    finished char counter

--- 100days/Javascript/Exercises/charcounter/app.js
+++ 100days/Javascript/Exercises/charcounter/app.js
@@ -4,16 +4,20 @@ let remainingCharsElement = document.getElementById('count');
 // these two variables ACCESS
 
 let maxAllowedChars = productNameInputElement.maxLength;
-// This dynamically assigns the max length allowed in the input element
+// This dynamically assigns the max length allowed that is set in the input element
 
 function updateRemainingChars(event) {
   let enteredText = event.target.value;
+  console.log(enteredText);
   let enteredTextLength = enteredText.length;
+  console.log(enteredTextLength);
 
   let remainingCharacters = maxAllowedChars - enteredTextLength;
+  console.log(remainingCharacters);
 
   remainingCharsElement.textContent = remainingCharacters;
+  console.log(remainingCharsElement.textContent);
 }
 
 productNameInputElement.addEventListener('input', updateRemainingChars);
-// input is a method and when it happens it will run the function updateRmainingChars
+// input is a method and when anything changes it will run the function updateRemainingChars
