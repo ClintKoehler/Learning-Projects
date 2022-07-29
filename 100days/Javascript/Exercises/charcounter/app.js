7d3ac08b948bd635c74a795061aaafa41948d337
Clint Koehler <clint.koehler@gmail.com>
2022-07-29 13:55:55 -0700

    finished number counter

--- 100days/Javascript/Exercises/charcounter/app.js
+++ 100days/Javascript/Exercises/charcounter/app.js
@@ -1,27 +1,28 @@
-// const spanElement = document.getElementById('count');
-
-// spanElement.classList.add('warning');
-// classList.add appends a class onto any that are already existing. className would replace it
-
 const productNameInputElement = document.getElementById('product-name');
 
 const remainingCharsElement = document.getElementById('count');
-// these two variables ACCESS
+
 const maxAllowedChars = productNameInputElement.maxLength;
-// This dynamically assigns the max length allowed that is set in the input element
 
 function updateRemainingChars(event) {
   const enteredText = event.target.value;
-  // console.log(enteredText); ==> letters entered
   const enteredTextLength = enteredText.length;
-  // console.log(enteredTextLength); ==> amount  of letters entered
-
   const remainingCharacters = maxAllowedChars - enteredTextLength;
-  // console.log(remainingCharacters); ==> 60 minus the amount of characters entered
 
   remainingCharsElement.textContent = remainingCharacters;
-  // console.log(remainingCharsElement.textContent); ==> assigns the value of remainingCharacters to the html count id element
+
+  if (remainingCharacters === 0) {
+    remainingCharsElement.classList.add('error');
+    productNameInputElement.classList.add('error');
+  } else if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add('warning');
+    productNameInputElement.classList.add('warning');
+    remainingCharsElement.classList.remove('error');
+    productNameInputElement.classList.remove('error');
+  } else {
+    remainingCharsElement.classList.remove('warning');
+    productNameInputElement.classList.remove('warning');
+  }
 }
 
 productNameInputElement.addEventListener('input', updateRemainingChars);
-// input is a method and when anything changes it will run the function updateRemainingChars
---
+++ 100days/Javascript/Exercises/charcounter/conditional-code.js
@@ -0,0 +1,5 @@
+const myName = 'Clint';
+
+if (myName === 'Clint') {
+  console.log('Hello!');
+}
