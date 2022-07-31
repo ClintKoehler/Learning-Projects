016af783d5d4587d1381b68fcd036436f76c6d77
Clint Koehler <clint.koehler@gmail.com>
2022-07-30 17:05:35 -0700

    working modal window for player 1 added

--- 100days/Javascript/tic-tac-toe/app.js
+++ 100days/Javascript/tic-tac-toe/app.js
@@ -1,11 +1,10 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById('myBtn');
-
+const btn = document.getElementById('player-one-edit');
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
+const cancel = document.getElementsByClassName('#cancel')[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -13,13 +12,13 @@ btn.onclick = function () {
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+cancel.onclick = function () {
   modal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = 'none';
+//   }
+// };
