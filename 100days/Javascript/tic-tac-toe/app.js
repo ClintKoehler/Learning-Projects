942f8947e7bee51834b290154e0d64756ffe254a
Clint Koehler <clint.koehler@gmail.com>
2022-07-31 16:10:43 -0700

    small changes to css

--- 100days/Javascript/tic-tac-toe/app.js
+++ 100days/Javascript/tic-tac-toe/app.js
@@ -17,8 +17,8 @@ cancel.onclick = function () {
 };
 
 // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = 'none';
-//   }
-// };
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
