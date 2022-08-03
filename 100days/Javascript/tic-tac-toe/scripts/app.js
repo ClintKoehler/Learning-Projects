a1393cbaab6b9688cd0f28255a127480f9dc5986
Clint Koehler <clint.koehler@gmail.com>
2022-08-03 15:50:09 -0700

    initial game js game logic working

--- 100days/Javascript/tic-tac-toe/scripts/app.js
+++ 100days/Javascript/tic-tac-toe/scripts/app.js
@@ -1,4 +1,11 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -16,14 +23,22 @@ const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
 const playerNameInput = document.getElementById('playername');
 const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const startNewGameElement = document.getElementById('start-btn');
 const editPlayer1BtnElement = document.getElementById('player-one-edit');
 const editPlayer2BtnElement = document.getElementById('player-two-edit');
 const cancelConfigBtnElement = document.getElementById('cancel');
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 formElement.addEventListener('submit', savePlayerConfig);
 startNewGameElement.addEventListener('click', startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener('click', selectGameField);
+// }
+gameBoardElement.addEventListener('click', selectGameField);
