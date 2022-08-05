684bcdda2781705a274230beed19f2018d60801c
Clint Koehler <clint.koehler@gmail.com>
2022-08-04 19:56:00 -0700

    learning js

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/app.js
@@ -0,0 +1,15 @@
+// let javascriptIsFun = true;
+// console.log(typeof javascriptIsFun);
+
+// 2**3 means 2 to the power of 3 (2 x 2 x 2 = 8)
+// //? ASSIGNMENT OPERATORS
+// let x = 10 + 5;
+// x += 10; // x = x + 10 so x is 15 + 10
+// x *= 4; // x = x * 4 so x is now 100
+// x++; // 101
+// x--; // 100
+// console.log(x)
+// //? COMPARISON OPERATORS
+// let myAge = 42;
+// const isFullAge = myAge >= 18;
+// console.log(isFullAge);
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,36 @@
+// const country = 'United Sates';
+// const continent = 'North America';
+// let population = 325500000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// const isIsland = false;
+// const language = 'english';
+
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof population);
+// console.log(typeof isIsland);
+// console.log(typeof language);
+
+// let populationSplit = population / 2;
+
+// console.log(populationSplit);
+// population++;
+// console.log(population)
+
+// let finlandPopulation = 6000000;
+// let moreOrLessThanFinland = (population > finlandPopulation);
+// console.log(moreOrLessThanFinland)
+
+// let averageCountryPopulation = 33000000;
+// let myCountryPopBiggerOrSmallerThanAverage = (population > averageCountryPopulation);
+// console.log(myCountryPopBiggerOrSmallerThanAverage);
+
+// let description = 'Portugal is in Europe, and its 11 million people speak portuguese'
+// console.log(description);
+
+// description = `Portugal is in Europe, and its 11 million people speak portuguese`;
+// console.log(description);
