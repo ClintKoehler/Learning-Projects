415d54ef8e600f44580cef81a111ee6c0eef9583
Clint Koehler <clint.koehler@gmail.com>
2022-08-06 18:29:47 -0700

    exercises

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/practice.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/practice.js
@@ -23,15 +23,63 @@
 // //? FUNCTION DECLARATIONS vs EXPRESSIONS
 
 //! function declaration, can invoke before declaration
-function calcAge1(birthYear) {
-  return 2022 - birthYear;
-}
-const age1 = calcAge1(1980);
+// function calcAge1(birthYear) {
+//   return 2022 - birthYear;
+// }
+// const age1 = calcAge1(1980);
 
 //! function expression, cannot invoke before declaration
-const calcAge2 = function (birthYear) {
-  return 2022 - birthYear;
-};
-const age2 = calcAge2(1990);
+// const calcAge2 = function (birthYear) {
+//   return 2022 - birthYear;
+// };
+// const age2 = calcAge2(1990);
 
-console.log(age1, age2);
+// console.log(age1, age2);
+
+// //? ARROW FUNCTIONS
+
+//! arrow function
+
+// const calcAge3 = (birthYear) => 2022 - birthYear;
+// // return is implicit (do not have to specify 'return' unless it is more than one line of code
+// const age3 = calcAge3(2000);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2022 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years`;
+// };
+
+// console.log(yearsUntilRetirement(1980, 'Clint'));
+
+// //? FUNCTIONS CALLING OTHER FUNCTIONS
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+// //? REVIEW
+
+// //! function declaration
+// function calcAge(birthYear) {
+//   return 2022 - birthYear;
+// }
+
+// //! function expression
+// const calcAge = function (birthYear) {
+//   return 2022 - birthYear;
+// };
+
+// //! arrow function
+// const calcAge = (birthYear) => 2022 - birthYear;
