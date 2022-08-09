65f75d9c287ea1bb822f6abceb2f6d083458224a
Clint Koehler <clint.koehler@gmail.com>
2022-08-09 11:09:26 -0700

    exercises

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/practice.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/practice.js
@@ -310,3 +310,24 @@ console.log(jonas.getSummary());
 // }
 
 // //? LOOPING BACKWARDS AND LOOPS IN LOOPS
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2022 - 1991,
+  'teacher',
+  ['Micheal', 'Peter', 'Steven'],
+  true,
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+//! loop inside a loop
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------- Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++)
+    console.log(`${exercise}: Lifting weigt repition ${rep}`);
+}
