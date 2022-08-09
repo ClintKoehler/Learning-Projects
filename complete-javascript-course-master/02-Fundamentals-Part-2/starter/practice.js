959da11dfac4c01048229155de6a3956a4dda02d
Clint Koehler <clint.koehler@gmail.com>
2022-08-09 15:38:41 -0700

    challenges

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/practice.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/practice.js
@@ -336,16 +336,16 @@ console.log(jonas.getSummary());
 
 // //? WHILE LOOP
 //! does not need a counter
-let rep = 1;
-while (rep <= 10) {
-  console.log(`While: Lifting weight repetition ${rep}`);
-  rep++;
-}
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`While: Lifting weight repetition ${rep}`);
+//   rep++;
+// }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
+// let dice = Math.trunc(Math.random() * 6) + 1;
 
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log(`You finally rolled a ${dice}!`);
-}
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log(`You finally rolled a ${dice}!`);
+// }
