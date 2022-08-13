b439b49f9eed724c9f086ba6bf82deaa6ee5a7f1
ClintKoehler <105824948+ClintKoehler@users.noreply.github.com>
2022-08-12 19:37:27 -0700

    pig game basic logic implemented

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,46 @@
 'use strict';
+
+let currentPlayer = 1;
+let currentScoreP1 = 0;
+let currentScoreP2 = 1;
+let totalScorep1 = 0;
+let totalScorep2 = 0;
+let turn = 0;
+
+document.querySelector('.btn--new').addEventListener('click', newGame);
+
+function newGame() {
+  document.getElementById('score--0').textContent = 0;
+  document.getElementById('score--1').textContent = 0;
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+}
+
+document.querySelector('.btn--roll').addEventListener('click', rollDice);
+// document.querySelector('.btn--hold').addEventListener('click,');
+
+function rollDice() {
+  if (currentPlayer === 1) {
+    playGameP1();
+  } else {
+    playGameP2();
+  }
+}
+
+function playGameP1() {
+  let diceRoll = Math.trunc(Math.random() * 6 + 1);
+  console.log(diceRoll);
+  console.log(currentPlayer);
+  currentPlayer = 2;
+}
+
+function playGameP2() {
+  let diceRoll = Math.trunc(Math.random() * 6 + 1);
+  console.log(diceRoll);
+  console.log(currentPlayer);
+  currentPlayer = 1;
+}
+
+function scoreCalcP1() {}
+
+function scoreCalcP2() {}
