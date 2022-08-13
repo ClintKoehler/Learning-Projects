3c896f14c6136efa227a43cff526da0484d9aee3
Clint Koehler <clint.koehler@gmail.com>
2022-08-13 15:42:11 -0700

    mostly complete pig game logic

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,50 +1,102 @@
 'use strict';
 
-let currentPlayer = 1;
+let currentPlayer = 0;
+let currentScoreP0 = 0;
 let currentScoreP1 = 0;
-let currentScoreP2 = 0;
-let totalScorep1 = 0;
-let totalScorep2 = 0;
+let totalScoreP0 = 0;
+let totalScoreP1 = 0;
+let turn = 0;
 
 document.querySelector('.btn--new').addEventListener('click', newGame);
+document.querySelector('.btn--roll').addEventListener('click', rollDice);
+document.querySelector('.btn--hold').addEventListener('click', hold);
 
 function newGame() {
   document.getElementById('score--0').textContent = 0;
   document.getElementById('score--1').textContent = 0;
   document.getElementById('current--0').textContent = 0;
   document.getElementById('current--1').textContent = 0;
-  currentPlayer = 1;
+  currentPlayer = 0;
+  currentScoreP0 = 0;
   currentScoreP1 = 0;
-  currentScoreP2 = 0;
-  totalScorep1 = 0;
-  totalScorep2 = 0;
+  totalScoreP0 = 0;
+  totalScoreP1 = 0;
+  turn = 1;
 }
 
-document.querySelector('.btn--roll').addEventListener('click', rollDice);
-// document.querySelector('.btn--hold').addEventListener('click,');
-
 function rollDice() {
-  if (currentPlayer === 1) {
+  if (turn === 0) {
+    newGame();
+  }
+  if (currentPlayer === 0) {
+    playGameP0();
+  } else if (currentPlayer === 1) {
     playGameP1();
+  } else return;
+}
+
+function playGameP0() {
+  let diceRoll = Math.trunc(Math.random() * 6 + 1);
+  console.log(diceRoll);
+  console.log('player', currentPlayer);
+  if (diceRoll === 1) {
+    currentScoreP0 = 0;
+    document.getElementById('current--0').textContent = 0;
+    currentPlayer = 1;
+    return;
   } else {
-    playGameP2();
+    currentScoreP0 += diceRoll;
+    document.getElementById('current--0').textContent = currentScoreP0;
+    if (currentScoreP0 >= 100) player0Wins();
   }
 }
 
 function playGameP1() {
   let diceRoll = Math.trunc(Math.random() * 6 + 1);
   console.log(diceRoll);
-  console.log(currentPlayer);
+  console.log('player', currentPlayer);
+  if (diceRoll === 1) {
+    currentScoreP1 = 0;
+    document.getElementById('current--1').textContent = 0;
+    currentPlayer = 0;
+    return;
+  } else {
+    currentScoreP1 += diceRoll;
+    document.getElementById('current--1').textContent = currentScoreP1;
+    if (currentScoreP1 >= 100) player1Wins();
+  }
+}
+
+function hold() {
+  if (currentPlayer === 0) {
+    totalScoreP0 = 100;
+    currentScoreP0 = 0;
+    document.getElementById('current--0').textContent = 0;
+    document.getElementById('score--0').textContent = totalScoreP0;
+    currentPlayer = 1;
+    if (totalScoreP0 >= 100) player0Wins();
+  } else if (currentPlayer === 1) {
+    totalScoreP1 += currentScoreP1;
+    currentScoreP1 = 0;
+    document.getElementById('current--1').textContent = 0;
+    document.getElementById('score--1').textContent = totalScoreP1;
+    currentPlayer = 0;
+    if (totalScoreP1 >= 100) player1Wins();
+  }
+}
+
+function player0Wins() {
+  document.getElementById('current--0').textContent = 'YOU WON!';
+  document.getElementById('current--1').textContent = 'YOU LOSE!';
+  document.getElementsByClassName('.current-label').textContent = 's';
   currentPlayer = 2;
+  return;
 }
 
-function playGameP2() {
-  let diceRoll = Math.trunc(Math.random() * 6 + 1);
-  console.log(diceRoll);
-  console.log(currentPlayer);
-  currentPlayer = 1;
+function player1Wins() {
+  document.getElementById('current--1').textContent = 'YOU WON!';
+  document.getElementById('current--0').textContent = 'YOU LOSE!';
+  document.querySelectorAll('.current-label').textContent = '';
+  currentPlayer = 2;
+  return;
 }
-
-function scoreCalcP1() {}
-
-function scoreCalcP2() {}
