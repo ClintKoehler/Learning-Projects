d255a6246b38a2e433e6ed314202ccdf4f58cebc
Clint Koehler <clint.koehler@gmail.com>
2022-08-19 19:19:26 -0700

    exercises

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -18,7 +18,8 @@ const openingHours = {
     open: 11,
     close: 23,
   },
-  [`day-${2 + 4}`]: {
+  // [`day-${2 + 4}`]: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -61,7 +62,63 @@ const restaurant = {
   },
 };
 
-console.log(restaurant.openingHours.mon);
+//? Property NAMES
+//? (remember) property names = keys
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day} `;
+}
+console.log(openStr);
+
+//? Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+//? ENTRIES return the index number AND elements
+//? Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+//? Optional chaining
+//? WITHOUT optional chaining
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+// if (restaurant.openingHours.fri) console.log(restaurant.openingHours.fri.open);
+
+//! below gives a hard error without optional chaining
+// console.log(restaurant.openingHours.mon.open);
+//? WITH optional chaining
+//! below returns undefined
+// only if .mon exist will .open be read
+// console.log(restaurant.openingHours.mon?.open);
+// below stops reading at openingHours
+// console.log(restaurant.openingHours?.mon?.open);
+
+//? EXAMPLE
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+//? Methods OPTIONAL CHAINING
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//? Arrays OPTIONAL CHAINING
+// const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+
+// basically this is if else
+// console.log(users[0]?.name ?? 'User array empty');
 
 //? FOR OF LOOPS
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
