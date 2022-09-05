3b93184c7436c9c94dd1c6129ab0318629a8c16e
Clint Koehler <clint.koehler@gmail.com>
2022-09-05 14:48:22 -0700

    jschallenger

--- complete-javascript-course-master/practice/app.js
+++ complete-javascript-course-master/practice/app.js
@@ -32,3 +32,80 @@
 // console.log(myFunction([10, 9, 8, 7, 6], 5)); //* 6
 
 //!
+// function myFunction(a, b) {
+//   return a < b ? a / b : a * b;
+// }
+
+// console.log(myFunction(10, 100)); //* 0.1
+
+//!
+// function myFunction(arr) {
+//   return arr.sort();
+// }
+
+// console.log(myFunction(['b', 'c', 'd', 'a'])); //* [a, b, c, d]
+
+//!
+// function myFunction(a) {
+//   return a.slice(3);
+// }
+
+// console.log(myFunction([5, 4, 3, 2, 1, 0])); //* [2, 1, 0]
+
+//!
+// function myFunction(a) {
+//   return a.slice(3);
+// }
+
+// console.log(myFunction('fgedcba')); //* dcba
+
+//!
+// function myFunction(a) {
+//   return a.slice(0, -3);
+// }
+
+// console.log(myFunction('fgedcba')); //* fged
+// console.log(myFunction('1234')); //* 1
+
+//!
+// function myFunction(a, b) {
+//   return (a / 100) * b;
+// }
+
+// console.log(myFunction(100, 50)); //* 50
+// console.log(myFunction(10, 1)); //* 0.1
+// console.log(myFunction(500, 25)); //* 125
+
+//!
+// function myFunction(a) {
+//   return a.slice(0, a.length / 2);
+// }
+
+// console.log(myFunction('abcdefgh')); //* abcd
+// console.log(myFunction('1234')); //* 12
+
+//!
+// function myFunction(a, b) {
+//   return a.includes(b) ? b + a : a + b;
+// }
+
+// //? Their way:
+// function myFunction(a, b) {
+//   return a.indexOf(b) === -1 ? a + b : b + a;
+// }
+
+// console.log(myFunction('cheese', 'cake')); //* cheesecake
+// console.log(myFunction('lips', 's')); //* slips
+// console.log(myFunction(' think, therefore I am', 'I')); //* I think, therefore I am
+
+//!
+function myFunction(a) {
+  return a.toFixed(2);
+}
+
+console.log(myFunction(2.12397)); //* 2.12
+console.log(myFunction(3.136)); //* 3.14
+console.log(myFunction(1.12397)); //* 1.12
+console.log(myFunction(26.1379)); //* 26.14
+
+//!
