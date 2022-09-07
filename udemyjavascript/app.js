6b44e60f84f1e85b89ea1c3e6bf67e4acf759de4
Clint Koehler <clint.koehler@gmail.com>
2022-09-06 19:21:19 -0700

    udemy js

--- udemyjavascript/app.js
+++ udemyjavascript/app.js
@@ -108,30 +108,53 @@
 //? great for getting unique values
 //? bad for values that are repeating
 //? Great for APIs
+// const people = [
+//   { name: 'bob', age: 20, position: 'developer', id: 1 },
+//   { name: 'peter', age: 25, position: 'designer', id: 2 },
+//   { name: 'susy', age: 30, position: 'the boss', id: 3 },
+//   { name: 'anna', age: 35, position: 'the boss', id: 4 },
+// ];
+
+// const names = ['bob', 'peter', 'suzy'];
+// console.log(
+//   names.find(function (name) {
+//     return name === 'bob';
+//   })
+// ); //* ==> bob because names is all strings
+
+// const person = people.find(function (person) {
+//   return person.id === 3;
+// });
+
+// console.log(person); //* ==> suzy object
+// console.log(person.name); //* ==> susy
+
+// const person2 = people.filter(function (person) {
+//   return person.id === 3;
+// });
+
+// console.log(person2); //* ==> susy array
+// console.log(person2[0].name); //* ==> susy
+
+//! //////////////REDUCE////////////////////
+//! ////////////////////////////////////////
+//? iterates, callback function
+//? reduces to a single value - number, array, object
+//? 1 param ('acc') - total of all calculations
+//? 2 param ('curr') - current iteration/value
 const people = [
-  { name: 'bob', age: 20, position: 'developer', id: 1 },
-  { name: 'peter', age: 25, position: 'designer', id: 2 },
-  { name: 'susy', age: 30, position: 'the boss', id: 3 },
-  { name: 'anna', age: 35, position: 'the boss', id: 4 },
+  { name: 'bob', age: 20, position: 'developer', id: 1, salary: 200 },
+  { name: 'peter', age: 25, position: 'designer', id: 2, salary: 300 },
+  { name: 'susy', age: 30, position: 'the boss', id: 3, salary: 500 },
+  { name: 'anna', age: 35, position: 'the boss', id: 4, salary: 500 },
 ];
 
-const names = ['bob', 'peter', 'suzy'];
-console.log(
-  names.find(function (name) {
-    return name === 'bob';
-  })
-); //* ==> bob because names is all strings
+const total = people.reduce(function (acc, currItem) {
+  //! ALWAYS RETURN THE ACCUMALATOR (ACC)
+  console.log(`total ${acc}`);
+  console.log(`current money: ${currItem.salary}`);
+  acc += currItem.salary;
+  return acc;
+}, 0);
 
-const person = people.find(function (person) {
-  return person.id === 3;
-});
-
-console.log(person); //* ==> suzy object
-console.log(person.name); //* ==> susy
-
-const person2 = people.filter(function (person) {
-  return person.id === 3;
-});
-
-console.log(person2); //* ==> susy array
-console.log(person2[0].name); //* ==> susy
+console.log(total); //* ==> 1500
