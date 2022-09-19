72e59604ef830892f421a0bf9424979469a32da2
Clint Koehler <clint.koehler@gmail.com>
2022-09-19 15:50:47 -0700

    TTT Modal Logic

--- 100days/Javascript/tic-tac-toe/scripts/app.js
+++ 100days/Javascript/tic-tac-toe/scripts/app.js
@@ -1,47 +1,11 @@
-const gameData = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
+const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
 
-let editedPlayer = 0;
-let activePlayer = 0;
-let currentRound = 1;
-let gameIsOver = false;
-
-const players = [
-  {
-    name: '',
-    symbol: 'X',
-  },
-  {
-    name: '',
-    symbol: 'O',
-  },
-];
-
-const playerConfigOverlayElement = document.getElementById('myModal');
-const formElement = document.querySelector('form');
-const errorsOutputElement = document.getElementById('config-errors');
-const playerNameInput = document.getElementById('playername');
-const gameAreaElement = document.getElementById('active-game');
-const activePlayerNameElement = document.getElementById('active-player-name');
-const gameOverElement = document.getElementById('game-over');
-
-const startNewGameElement = document.getElementById('start-btn');
-const editPlayer1BtnElement = document.getElementById('player-one-edit');
-const editPlayer2BtnElement = document.getElementById('player-two-edit');
-const cancelConfigBtnElement = document.getElementById('cancel');
-// const gameFieldElements = document.querySelectorAll('#game-board li');
-const gameBoardElement = document.getElementById('game-board');
+const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
+const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
-formElement.addEventListener('submit', savePlayerConfig);
-startNewGameElement.addEventListener('click', startNewGame);
-
-// for (const gameFieldElement of gameFieldElements) {
-//   gameFieldElement.addEventListener('click', selectGameField);
-// }
-gameBoardElement.addEventListener('click', selectGameField);
+backdropElement.addEventListener('click', closePlayerConfig);
--- 100days/Javascript/tic-tac-toe/scripts/config.js
+++ 100days/Javascript/tic-tac-toe/scripts/config.js
@@ -1,37 +1,11 @@
-function openPlayerConfig(event) {
-  const selectedPlayerId = event.target.dataset.playerid;
-  editedPlayer = +selectedPlayerId; // value of 1 or 2, the + converts to a number
+//! sets modal and overaly from hidden to block
+function openPlayerConfig() {
   playerConfigOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
 }
 
+//! set modal and overlay to hidden
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
-  document.getElementById('label-name').classList.remove('error');
-  document.getElementById('playername').classList.remove('error');
-  errorsOutputElement.textContent = '';
-  formElement;
-  formElement.children[2].value = '';
-}
-
-function savePlayerConfig(event) {
-  event.preventDefault(); // browser will not submit to server. Page will no longer reload
-  const formData = new FormData(event.target); // generate object. target points at <form>
-  const enteredPlayerName = formData.get('username').trim();
-
-  if (!enteredPlayerName) {
-    // enteredPlayerName === ''
-    document.getElementById('label-name').classList.add('error');
-    document.getElementById('playername').classList.add('error');
-    errorsOutputElement.textContent = 'Please enter a valid name!';
-    return; // stops execution if nothing entered
-  }
-
-  const updatedPlayerDataElement = document.getElementById(
-    'player-' + editedPlayer
-  );
-  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
-
-  players[editedPlayer - 1].name = enteredPlayerName;
-
-  closePlayerConfig(); // executes manually due to ()
+  backdropElement.style.display = 'none';
 }
