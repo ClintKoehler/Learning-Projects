97d6f339da15fb7482c451127f6fbcb3c82cf0c7
Clint Koehler <clint.koehler@gmail.com>
2022-09-19 17:43:37 -0700

    Form Buttons Initial

--- 100days/Javascript/tic-tac-toe/scripts/config.js
+++ 100days/Javascript/tic-tac-toe/scripts/config.js
@@ -1,11 +1,31 @@
-//! sets modal and overaly from hidden to block
+//? sets modal and overaly from hidden to block
 function openPlayerConfig() {
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
 }
 
-//! set modal and overlay to hidden
+//? set modal and overlay to hidden
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
+}
+
+//? get the player name and save into a const using FormData and .get
+//? preventDefault keeps submit button from sending http
+//? -request causing a page reload
+//? target is the form html element that caused the event
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername').trim();
+
+  //? Empty string is considered falsey
+  //? Could also use enteredPlayername === ''
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return; //? Will stop function if this code is run
+  }
 }
