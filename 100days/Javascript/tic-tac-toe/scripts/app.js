2680d45dbff6eb476908daaeb8ac64da81cc5932
Clint Koehler <clint.koehler@gmail.com>
2022-09-20 14:23:16 -0700

    TTT Game Board Initial

--- 100days/Javascript/tic-tac-toe/scripts/app.js
+++ 100days/Javascript/tic-tac-toe/scripts/app.js
@@ -1,15 +1,31 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: '',
+    symbol: 'X',
+  },
+  {
+    name: '',
+    symbol: 'O',
+  },
+];
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form'); // selects first 'form' element
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+const startNewGameBtnElement = document.getElementById('start-game-btn');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+startNewGameBtnElement.addEventListener('click', startNewGame);
 
 formElement.addEventListener('submit', savePlayerConfig);
--- 100days/Javascript/tic-tac-toe/scripts/config.js
+++ 100days/Javascript/tic-tac-toe/scripts/config.js
@@ -1,5 +1,8 @@
 //? sets modal and overaly from hidden to block
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  //? dataset.playerid knows if p1 or p2 buttons were clicked
+  //? + converts string to number
+  editedPlayer = +event.target.dataset.playerid;
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
 }
@@ -10,14 +13,16 @@ function closePlayerConfig() {
   backdropElement.style.display = 'none';
   formElement.firstElementChild.classList.remove('error');
   errorsOutputElement.textContent = '';
+  document.getElementById('playername').value = '';
 }
 
-//? get the player name and save into a const using FormData and .get
-//? preventDefault keeps submit button from sending http
-//? -request causing a page reload
-//? target is the form html element that caused the event
+//? get the player name and save into a variable using FormData and .get
+//? update the player name dynamically
 function savePlayerConfig(event) {
+  //? preventDefault keeps submit button from sending http
+  //? -request causing a page reload
   event.preventDefault();
+  //? target is the form html element that caused the event
   const formData = new FormData(event.target);
   const enteredPlayername = formData.get('playername').trim();
 
@@ -28,4 +33,14 @@ function savePlayerConfig(event) {
     errorsOutputElement.textContent = 'Please enter a valid name!';
     return; //? Will stop function if this code is run
   }
+
+  const updatedPlayerDataElement = document.getElementById(
+    'player-' + editedPlayer + '-data'
+  );
+  //? DOM traversal method below
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
 }
